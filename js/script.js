55104129d76ed1fde460e1a2c9a81e796d3bcb6f
Maxzb <47795883+Maxzb@users.noreply.github.com>
2019-11-14 01:03:06 +0300

    14.11.2019/2

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 $('.about-me-link').click(function() {
-   $('.about-main-block').css('margin-left', '0');
+   $('.close').toggleClass('active');
+   $('.about-wrap-block').toggleClass('active-wrap');
+   $('.cop').toggleClass('color-cop');
+   $('.about-me-info-block').toggleClass('margin-right');
+   $('.cop-rect, .offer-block, .portfolio-block, .social-block').toggleClass('z-index');
+});
+$('.resume-link').click(function() {
+   $('.close').toggleClass('active');
+   $('.about-wrap-block').toggleClass('active-wrap');
+   $('.cop').toggleClass('color-cop');
+   $('.about-me-info-block').toggleClass('margin-right');
+   $('.cop-rect, .offer-block, .portfolio-block, .social-block').toggleClass('z-index');
 });
