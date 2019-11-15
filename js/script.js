f4c990a3e474fd1ec2fc45ec27b9c67a263669de
Maxzb <47795883+Maxzb@users.noreply.github.com>
2019-11-15 09:28:43 +0300

    14.11.2019

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
-$('.about-me-link').click(function() {
-   $('.close').toggleClass('active');
-   $('.about-wrap-block').toggleClass('active-wrap');
-   $('.cop').toggleClass('color-cop');
-   $('.about-me-info-block').toggleClass('margin-right');
-   $('.cop-rect, .offer-block, .portfolio-block, .social-block').toggleClass('z-index');
-});
-$('.resume-link').click(function() {
-   $('.close').toggleClass('active');
-   $('.about-wrap-block').toggleClass('active-wrap');
-   $('.cop').toggleClass('color-cop');
-   $('.about-me-info-block').toggleClass('margin-right');
-   $('.cop-rect, .offer-block, .portfolio-block, .social-block').toggleClass('z-index');
+$(document).ready(function(){
+   $('.about-me-link').click(function() {
+      $('.close').toggleClass('active');
+      $('.about-wrap-block').toggleClass('active-wrap');
+      $('.cop').toggleClass('color-cop');
+      $('.about-me-info-block').toggleClass('margin-right');
+      $('.cop-rect, .offer-block, .portfolio-block, .social-block').toggleClass('z-index');
+   });
+   $('.resume-link').click(function() {
+      $('.close').toggleClass('active');
+      $('.about-wrap-block').toggleClass('active-wrap');
+      $('.cop').toggleClass('color-cop');
+      $('.about-me-info-block').toggleClass('margin-right');
+      $('.cop-rect, .offer-block, .portfolio-block, .social-block').toggleClass('z-index');
+   });
 });
